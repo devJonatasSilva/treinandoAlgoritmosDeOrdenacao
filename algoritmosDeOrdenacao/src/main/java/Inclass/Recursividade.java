bceac1928bcd7c43db25f832eeacabc218a25764
jony_ <jony_@DESKTOP-B7MOECE>
2021-05-25 20:02:52 -0300

    exercicios de fixação em aula

---
+++ algoritmosDeOrdenacao/src/main/java/Inclass/Recursividade.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Inclass;
+
+/**
+ *
+ * @author Jonatas
+ */
+public class Recursividade {
+    
+    public static void main(String[] args) {
+        mensagemRecursiva("olá mundo", 5);
+    }
+    
+    public static void mensagemRecursiva(String mensagem, int contador){
+        if(contador > 0){
+            System.out.println("loop -> " + contador);
+            System.out.println(mensagem);
+            contador--;
+            mensagemRecursiva(mensagem, contador);
+        }
+    }   
+}
